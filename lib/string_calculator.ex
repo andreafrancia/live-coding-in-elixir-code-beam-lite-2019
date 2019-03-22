759f23f3d4e13b319e14f880cc282d224bec86d3
Andrea Francia <andrea@andreafrancia.it>
2019-03-22 11:39:01 +0100

    Save.

--- lib/string_calculator.ex
+++ lib/string_calculator.ex
@@ -1,18 +1,21 @@
 defmodule StringCalculator do
-  @moduledoc """
-  Documentation for StringCalculator.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> StringCalculator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def sum_of("//" <> rest) do
+    [custom_separator, text] = String.split(rest, "\n")
+    sum_of(text, [custom_separator])
   end
+  def sum_of(text, custom_separators \\ []) do
+    text
+    |> split_string(custom_separators)
+    |> convert_to_integer
+    |> Enum.sum
+  end
+  def split_string(text, custom_separators) do
+    text
+    |> String.split([",", "\n"] ++ custom_separators, trim: true)
+  end
+  def convert_to_integer(list) do
+    list
+    |> Enum.map(&String.to_integer/1)
+  end
+
 end
--- test/string_calculator_test.exs
+++ test/string_calculator_test.exs
@@ -1,8 +1,44 @@
+defmodule StringCalculatorTestMacro do
+  defmacro test_sum_of(text, is: expected_value) do
+    message = text
+    ast = quote do
+      test unquote(message) do
+        assert unquote(expected_value) == sum_of(unquote(text))
+      end
+    end
+    ast
+  end
+end
 defmodule StringCalculatorTest do
   use ExUnit.Case
-  doctest StringCalculator
+  import StringCalculator
+  import StringCalculatorTestMacro
 
-  test "greets the world" do
-    assert StringCalculator.hello() == :world
+  test "how to define a macro" do
+    ast = quote do 
+      test_sum_of("1", is: 1)
+    end
+    ast = Macro.expand_once(ast, __ENV__)
+    assert "test(\"1\") do\n  assert(1 == sum_of(\"1\"))\nend" == 
+      Macro.to_string(ast)
+  end
+
+  describe "sum of an empty string is 0" do
+    test_sum_of("", is: 0)
+  end
+  describe "the sum of a string with one number is the number" do
+    test_sum_of("1", is: 1)
+    test_sum_of("2", is: 2)
+  end
+  describe "the sum of a string with comma separated values is the sum of .." do
+    test_sum_of("1,2", is: 3)
+    test_sum_of("1,2,3", is: 6)
+  end
+  describe "it works also with new lines as separator" do
+    test_sum_of("1\n3", is: 4)
+  end
+  describe "custom separator" do
+    test_sum_of("//_\n1_2", is: 3)
+    test_sum_of("//?\n4?2", is: 6)
   end
 end
